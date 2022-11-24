896bd92d95d886dec8b7c6671ad92822e02daaf3
Zane Shaw <admin@triangle100.com>
2022-11-24 23:18:32 +1100

    Add Holopin board

--- src/lib/Holopin.svelte
+++ src/lib/Holopin.svelte
@@ -1,1 +1,18 @@
-<h1>Holopin</h1>
+<script lang="ts">
+	/** The [Holopin](https://holopin.io/) user. */
+	export let user: string;
+</script>
+
+<!--
+	@component
+	Displays the desired user's [Holopin](https://holopin.io/) board (100% width).
+
+	Usage:
+
+	```tsx
+	<Holopin user="zaneshaw">
+	```
+-->
+<a href="https://holopin.io/@{user}">
+	<img src="https://holopin.me/{user}" alt="@{user}'s Holopin board" style="width: 100%" />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,6 @@
 	import Holopin from '$lib/Holopin.svelte';
 </script>
 
-<Holopin />
+<div style="width: 1000px;">
+	<Holopin user={'zaneshaw'} />
+</div>
