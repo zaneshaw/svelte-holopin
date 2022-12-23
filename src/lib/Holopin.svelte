9dd523ad7e6a03ff7bd35db19173dfcd807b9c39
Zane Shaw <admin@triangle100.com>
2022-12-24 07:50:48 +1100

    Update alt text
    Update alt text to be more descriptive and follow Holopin's standards closer

--- src/lib/Holopin.svelte
+++ src/lib/Holopin.svelte
@@ -5,7 +5,7 @@
 
 <!--
 	@component
-	Displays the desired user's [Holopin](https://holopin.io/) board (100% width).
+	An image of the desired user's [Holopin](https://holopin.io/) badges, which is a link to view their full Holopin profile (100% width)
 
 	Usage:
 
@@ -14,7 +14,7 @@
 	```
 -->
 <a href="https://holopin.io/@{user}">
-	<img src="https://holopin.me/{user}" alt="@{user}'s Holopin board" />
+	<img src="https://holopin.me/{user}" alt="An image of @{user}'s Holopin badges, which is a link to view their full Holopin profile" />
 </a>
 
 <style>
